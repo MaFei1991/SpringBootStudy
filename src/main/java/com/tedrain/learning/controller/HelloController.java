11798997d272a1851aa676c4f5658c1c6fb4b460
mazasan <2567346284@qq.com>
2020-03-28 10:54:19 +0800

    编写Hello,Word，启用热部署

---
+++ src/main/java/com/tedrain/learning/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.tedrain.learning.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/*
+* @RestController 注解说明 controller 里面的方法都以json格式输出，不用写jackjson配置
+* @Controller 普通页面
+ * */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello,SpringBoot!I";
+    }
+}
